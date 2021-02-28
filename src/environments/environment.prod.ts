a31cd4c1708b7a5d43a2f288f9470676304cbf40
Jason Fisher <jason@wanax.com>
2021-02-28 13:17:22 -0500

    wired up new Firebase hooks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,11 @@ export const environment = {
   production: true,
   server: 'https://somethingsomething.herokuapp.com',
   firebaseConfig: {
-    apiKey: 'AIzaSyCo1xG-lDjwUsXAMQPmFKPuac_7davNc_A',
-    authDomain: 'csci-260-2021.firebaseapp.com',
-    databaseURL: 'https://csci-260-2021.firebaseio.com',
-    projectId: 'csci-260-2021',
-    storageBucket: 'csci-260-2021.appspot.com',
-    messagingSenderId: '819938677496',
-    appId: '1:819938677496:web:53411aabc0021b9a08e10d'
+    apiKey: "AIzaSyCcM-20pL4E8a31iUuEu7JVHBl0GZYhV3Q",
+    authDomain: "csci-260-2021-events.firebaseapp.com",
+    projectId: "csci-260-2021-events",
+    storageBucket: "csci-260-2021-events.appspot.com",
+    messagingSenderId: "378184929240",
+    appId: "1:378184929240:web:7088da32d50e9e95b6ecad"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,12 @@ export const environment = {
   production: false,
   server: 'http://localhost:3000',
   firebaseConfig: {
-    apiKey: 'AIzaSyCo1xG-lDjwUsXAMQPmFKPuac_7davNc_A',
-    authDomain: 'csci-260-2021.firebaseapp.com',
-    databaseURL: 'https://csci-260-2021.firebaseio.com',
-    projectId: 'csci-260-2021',
-    storageBucket: 'csci-260-2021.appspot.com',
-    messagingSenderId: '819938677496',
-    appId: '1:819938677496:web:53411aabc0021b9a08e10d'
+    apiKey: "AIzaSyCcM-20pL4E8a31iUuEu7JVHBl0GZYhV3Q",
+    authDomain: "csci-260-2021-events.firebaseapp.com",
+    projectId: "csci-260-2021-events",
+    storageBucket: "csci-260-2021-events.appspot.com",
+    messagingSenderId: "378184929240",
+    appId: "1:378184929240:web:7088da32d50e9e95b6ecad"
   }
 };
 
