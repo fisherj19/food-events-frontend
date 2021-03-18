e155858088bb96bfd2bfaf6ebc6855654d930697
madicatalano <catalanom@xavier.edu>
2021-03-17 22:33:20 -0400

    starting to create /account page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './views/home.component';
 import { LoginComponent } from './views/login.component';
 import { VerifyComponent } from './views/verify.component';
 
+
 const notLoggedIn = () => map(user => user ? ['home'] : true);
 
 const routes: Routes = [
@@ -14,7 +15,9 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: notLoggedIn } },
   { path: 'verify', component: VerifyComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: !notLoggedIn } },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: 'account'}
+ 
 ];
 
 @NgModule({
