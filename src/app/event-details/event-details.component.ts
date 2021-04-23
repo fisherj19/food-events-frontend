bf32c15ac91c279c77a5c60fd3e19457aac406c2
jripperger <rippergerjacob@gmail.com>
2021-04-23 00:33:05 -0400

    event details update 4 --working

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -14,7 +14,7 @@ export class EventDetailsComponent implements OnInit {
   startTime: Date;
   endTime: Date;
   location: string;
-  gluten_free: boolean;
+  glutenFree: boolean;
   halal: boolean;
   kosher: boolean;
   vegetarian: boolean;
@@ -23,25 +23,26 @@ export class EventDetailsComponent implements OnInit {
   constructor(
     public dialog: MatDialog,
     public dialogRef: MatDialogRef<EventDetailsComponent>,
-    public e: FoodEvent[],
-    @Inject(MAT_DIALOG_DATA) private readonly data: FoodEvent[]
+    @Inject(MAT_DIALOG_DATA) private readonly data: FoodEvent
   ) { }
 
   ngOnInit(): void {
-    this.name = this.data.e.event_name;
-    this.description = this.data.e.event_desc;
-    this.date = this.data.e.event_date;
-    this.startTime = this.data.e.food_start_time;
-    this.endTime = this.data.e.food_end_time;
-    this.location = this.data.e.event_location;
-    this.gluten_free = this.data.e.gluten_free;
-    this.halal = this.data.e.halal;
-    this.kosher = this.data.e.kosher;
-    this.vegetarian = this.data.e.vegetarian;
-    this.vegan = this.data.e.vegan;
+    this.name = this.data.event_name;
+    this.description = this.data.event_desc;
+    this.date = this.data.event_date;
+    this.startTime = this.data.food_start_time;
+    this.endTime = this.data.food_end_time;
+    this.location = this.data.event_location;
+    this.glutenFree = this.data.gluten_free;
+    this.halal = this.data.halal;
+    this.kosher = this.data.kosher;
+    this.vegetarian = this.data.vegetarian;
+    this.vegan = this.data.vegan;
   }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  
 }
